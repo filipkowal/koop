c6bdbe93fc06993c766135990432473396af5caa
filipkowal <filip.kowal@protonmail.com>
2016-05-31 21:04:52 +0200

    Methods, Secure
    All operations on databases are now in meteor.methods;
    collections are published and subscribed

--- kooperatywa1.2.1.js
+++ kooperatywa1.2.1.js
@@ -1,24 +1,33 @@
+// Zrób scheme, żeby methods przyjmowały tylko odpowiednie typy danych
+// Stwórz typy użytkowników i do walidacje po typach http://guide.meteor.com/accounts.html#roles-and-permissions
+//routing https://kadira.io/academy/meteor-routing-guide/content/implementing-auth-logic-and-permissions
+//blahbalbh
 Products = new Mongo.Collection("products");
 Orders = new Mongo.Collection("orders");
 
 if (Meteor .isClient) {
-  
+  Meteor.subscribe('Products');
+  Meteor.subscribe('Orders');
+
   Template.productsList.helpers({
-    showProducts: function () {
+    //List the products from the Products collection
+    showProducts() {
       var sortAttr = Session.get('sortAttr');
       var sortOrder = Session.get('sortOrder') || 1;
       return Products.find({}, { sort: {[sortAttr]: sortOrder} });
     },
-    nameTriangle: function () {
+
+    //TABLE SORTING
+    nameTriangle() {
       if (Session.get('sortAttr') === 'productName') return true;
     },
-    producerTriangle: function() {
+    producerTriangle() {
       if (Session.get('sortAttr') === 'producer') return true;
     },
-    priceTriangle: function() {
+    priceTriangle() {
       if (Session.get('sortAttr') === 'price') return true;
     },
-    sortOrder: function(){
+    sortOrder(){
       if (Session.get('sortOrder') === 1) {
         return '&blacktriangle;';
       }
@@ -29,7 +38,8 @@ if (Meteor .isClient) {
   });
 
   Template.productsList.events({
-    "submit .orderProduct": function (event) {
+    "submit .orderProduct"(event) {
+      //Submit a product to Orders collection
       event.preventDefault();
       var quantity = event.target.quantity.value;
       var productName = this.productName;
@@ -39,38 +49,27 @@ if (Meteor .isClient) {
       var price = this.price;
       var summedPrice = price * quantity;
 
-      if(!(Orders.find(this._id).fetch().length)) {
-        Orders.insert({
-        _id: productsId,
-        productName: productName,
-        producer: producer,
-        quantity: quantity,
-        unit: unit,
-        createdAt: new Date(),
-        purchaser: Meteor.userId(),
-        price: price,
-        summedPrice: summedPrice
-        });   
-      } else {
-        Orders.update(productsId, {$set: {quantity: quantity, summedPrice: summedPrice}});
-        }
+      Meteor.call('insertOrder', productsId, productName, producer, quantity, unit,
+       price, summedPrice);
     },
-    "click .delete": function () {
-      if (confirm("UsunÄ…Ä‡ produkt?")){
+    "click .delete"() {
+      if (confirm("Usunąć produkt?")){
         Products.remove(this._id);
       }
     },
-    "click .name": function() {
+
+    //TABLE SORTING
+    "click .name" () {
       Session.set('sortNameOrder', Session.get('sortOrder') * -1 || -1);
       Session.set('sortAttr', 'productName');
       Session.set('sortOrder', Session.get('sortNameOrder'));
     },
-    "click .producer": function() {
+    "click .producer"() {
       Session.set('sortProducerOrder', Session.get('sortOrder') * -1 || -1);
       Session.set('sortAttr', 'producer');
       Session.set('sortOrder', Session.get('sortProducerOrder'));
     },
-    "click .price": function() {
+    "click .price"() {
       Session.set('sortPriceOrder', Session.get('sortOrder') * -1 || -1);
       Session.set('sortAttr', 'price');
       Session.set('sortOrder', Session.get('sortPriceOrder'));
@@ -78,22 +77,22 @@ if (Meteor .isClient) {
   });
 
   Template.ordersList.helpers({
-    showOrders: function () {
+    showOrders() {
       return Orders.find({}, {sort: {createdAt: -1}});
     },
-    orderSum: function() {
+    orderSum() {
       console.log(Orders.find().summedPrice);
     }
   });
 
   Template.ordersList.events({
-    "click .delete": function () {
-      Orders.remove(this._id)
+    "click .delete"() {
+      Meteor.call('deleteOrder', this._id);
     }
   });
 
   Template.insertProduct.events({
-    "submit .insertProduct": function (event) {
+    "submit .insertProduct"(event) {
         event.preventDefault();
         var productName = event.target.productName.value;
         var producer = event.target.producer.value;
@@ -102,18 +101,59 @@ if (Meteor .isClient) {
         var owner = Meteor.userId();
         var unit = event.target.unit.value;
 
-        Products.insert({
-        productName: productName,
-        producer: producer,
-        price: price,
-        createdAt: createdAt,
-        owner: owner,
-        unit: unit
-        });
+        Meteor.call('insertProduct', productName, producer, price, createdAt, owner, unit);
 
+        //Clear textboxes in the form
         event.target.productName.value = "";
         event.target.producer.value = "";
         event.target.price.value = "";
-        } 
-    });
+    },
+  });
 }
+
+if (Meteor.isServer) {
+  Meteor.publish('Products', function(){
+    return Products.find();
+  });
+  Meteor.publish('Orders', function(){
+    return Orders.find({ purchaser: this.userId });
+  });
+}
+
+Meteor.methods({
+  insertProduct(productName, producer, price, createdAt, owner, unit) {
+    //If user is logged
+    if(Meteor.userId()) {
+      Products.insert({
+      productName: productName,
+      producer: producer,
+      price: price,
+      createdAt: createdAt,
+      owner: owner,
+      unit: unit
+    });
+    }
+  },
+  insertOrder(productsId, productName, producer, quantity, unit, price, summedPrice){
+    Orders.upsert({
+        _id: productsId
+      }, 
+        {$setOnInsert: {
+          _id: productsId,
+          productName: productName,
+          producer: producer,
+          unit: unit,
+          price: price,
+          purchaser: Meteor.userId(),
+        },
+         $set: {
+            quantity: quantity,
+            createdAt: new Date(),
+            summedPrice: summedPrice}}
+    );
+
+  },
+  deleteOrder(orderId){
+     Orders.remove(orderId);
+  },
+});
